5517efa1f054be33915213c98f4cbc1a2e9d5bd0
saoudahm <saoudet.ahmed@gmail.com>
2017-01-29 23:24:23 +0100

    add AttaquantPlus
    add AttaquantPlus

--- tools.py
+++ tools.py
@@ -24,10 +24,13 @@ class MyState(object):
 		self.but_adv = Vector2D(150, 45) if self.key[0] == 1 else Vector2D(0, 45)	
 		#recup joueur 
 		self.all_players = self.state.players
-		self.co_players = [ p  for p in self.all_players if p[0] == self.key[0]]
+		self.co_players = [p  for p in self.all_players if (p[0] == self.key[0] and p[1] != self.key[1])]
 		self.adv_players = [p  for p in self.all_players if p[0] != self.key[0]]
 		#can the player shoot in the ball
 		self.can_shoot = True if self.my_position.distance(self.ball_position) < 0.82 else False
+		
+		#est proche de la balle
+		self.near_ball = True if self.my_position.distance(self.ball_position) < 20 else False
 	
 	def aller(self, p) :
 		return SoccerAction(p-self.my_position, Vector2D())
@@ -47,6 +50,10 @@ class MyState(object):
 			if self.my_position.distance(self.state.player_state(p[0], p[1]).position) < self.my_position.distance(self.state.player_state(pp[0], pp[1]).position):
 				pp = p
 		return self.state.player_state(pp[0], pp[1])
+	
+	#true if player p near ball
+	def p_near_ball(self, p):
+		return True if self.ball_position.distance(self.state.player_state(p[0], p[1]).position) < 20 else False
 				
 		
 ## Strategie aleatoire
@@ -66,6 +73,22 @@ class Attaquant(Strategy):
 		#return mstate.adv_nearby()
 		return mstate.aller(mstate.ball_position) + mstate.shoot(mstate.but_adv)
 
+
+class AttaquantPlus(Strategy):
+	def __init__(self, name="attaquantPlus"):
+		Strategy.__init__(self, name)
+	def compute_strategy(self, state, idteam, idplayer):
+		mstate = MyState(state, idteam, idplayer)
+		
+		for p in mstate.co_players:
+	#if un autre joueur proche de la balle et la balle au dela de la moitie du terrainaller de lavant
+			if  mstate.p_near_ball(p) and mstate.ball_position.distance(mstate.but_adv) < 50:
+				print mstate.state.player_state(p[0], p[1]).name
+				return Vector2D(1,0)
+			elif mstate.can_shoot :
+				return mstate.shoot(mstate.but_adv)
+			
+		#return mstate.aller(mstate.ball_position) + mstate.shoot(mstate.but_adv)
 
 		
 class Defenseur(Strategy):
