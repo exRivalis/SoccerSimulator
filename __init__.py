c8d3d35f179f406dfb604c37cee1a18a31eb4bbf
SAOUDET <saoudet.ahmed@gmail.com>
2017-02-03 18:45:11 +0100

    une deux

--- __init__.py
+++ __init__.py
@@ -19,7 +19,7 @@ from tools import MyState
 from strategy import RandomStrategy, Attaquant, AttaquantPlus, Defenseur
 
 joueur1 = Player("player1", Attaquant())
-joueur2 = Player("player2", Defenseur())
+joueur2 = Player("player2", Attaquant())
 
 joueur3 = Player("player3", Attaquant())
 joueur4 = Player("player4", AttaquantPlus())
@@ -28,4 +28,4 @@ team1 = SoccerTeam("Eq1", [joueur1])
 
 
 
-team2 = SoccerTeam("Eq2", [joueur3, joueur4])
+team2 = SoccerTeam("Eq2", [joueur1, joueur2])
