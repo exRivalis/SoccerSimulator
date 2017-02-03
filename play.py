3c69ca2352f2837588328e11ec90e2515ac138ba
exrivalis <saoudet.ahmed@gmail.com>
2017-02-03 20:48:29 +0100

    ajout play.py

---
+++ play.py
@@ -0,0 +1,40 @@
+#les objets de base
+from soccersimulator import Vector2D, SoccerState, SoccerAction
+
+#objets pour un match
+from soccersimulator import Simulation, SoccerTeam, Player, show_simu, SoccerTournament
+
+#importer la strategie de base
+from soccersimulator import Strategy
+
+#toutes les constantes
+from soccersimulator import settings
+
+#module math
+import math
+
+#toolbox
+from tools import MyState
+
+from strategy import RandomStrategy, Attaquant, AttaquantPlus, Defenseur, DefenseurPlus
+
+
+
+#simulation
+
+joueur1 = Player("player1", Attaquant())
+joueur2 = Player("player2", Attaquant())
+
+joueur3 = Player("player3", Attaquant())
+joueur4 = Player("player4", AttaquantPlus())
+
+
+team1 = SoccerTeam("Eq1", [joueur1, joueur2])
+team2 = SoccerTeam("Eq2", [joueur3, joueur4])
+
+match = Simulation(team1,team2, 2000)
+
+#print team1.players
+
+show_simu(match)
+
